e0ccf7ab683e5de54245c689b1f4bbb578af0c8b
sam <samuel.j.m.cameron@gmail.com>
2020-01-17 17:07:34 -0400

    still writing up results of calculation.

--- docs/N-particle-coarse-grain/compare_numerical_to_analytical.py
+++ docs/N-particle-coarse-grain/compare_numerical_to_analytical.py
@@ -16,12 +16,29 @@ if __name__=="__main__":
     Wps = Wp(ts,l0)
 
 
-    fig,axarr = plt.subplots(2)
+    fig,axarr = plt.subplots(2,sharex=True)
+
+    fig.set_size_inches(3.6,5)
 
     axarr[0].plot(ts,ys[:,0],'r.',label='numeric')
     axarr[0].plot(ts,Ws,'k-',label='analytic')
+    axarr[0].set_xlabel(r'$r$')
+    axarr[0].set_ylabel(r'$W_h(r)$')
+    axarr[0].text(20,-0.2,r'$\Pi=0.1$')
 
+    axarr[0].text(-0.05, 1.1, '(a)', transform=axarr[0].transAxes,
+                  fontsize=10, fontweight='bold', va='top', ha='right')
+    
     axarr[1].plot(ts,ys[:,1],'r.',label='numeric')
     axarr[1].plot(ts,Wps,'k-',label='analytic')
+    axarr[1].set_xlabel(r'$r$',fontsize=10)
+    axarr[1].set_ylabel(r'$W_h^{\prime}(r)$',fontsize=10)
+    axarr[1].legend(frameon=False,fontsize=10)
+    axarr[1].text(-0.05, 1.1, '(b)', transform=axarr[1].transAxes,
+                  fontsize=10, fontweight='bold', va='top', ha='right')
 
+
+    fig.subplots_adjust(left=0.2,right=0.9)
+    fig.savefig('Figures/fig1.pdf')
+    
     plt.show()
--- docs/N-particle-coarse-grain/compile_tex.sh
+++ docs/N-particle-coarse-grain/compile_tex.sh
@@ -6,7 +6,7 @@
 mkdir -p intfiles # if no subdirectory to store intermediate files, then
 #                   make the subdirectory
 
-pdflatex -halt-on-error --output-directory=intfiles $1.tex\
+pdflatex -halt-on-error $1tex\
 | grep -a3 ^! # grep -a3 ^! checks if there is a ! three lines up from error
 greprc=$? # if grep finds something (i.e. error occurs, then return value
 #           is currently at 0, if it doesn't find anything then return
@@ -16,15 +16,15 @@ if [[ $greprc -eq 0 ]] ; then
     exit 1
 fi
 echo First compilation of pdflatex successful
-bibtex intfiles/$1.aux
-pdflatex -halt-on-error --output-directory=intfiles $1.tex\
+bibtex $1aux 
+pdflatex -halt-on-error $1tex\
 | grep -a3 ^!
 greprc=$?
 if [[ $greprc -eq 0 ]] ; then
     exit 1
 fi
 echo Second compilation of pdflatex successful
-pdflatex -halt-on-error --output-directory=intfiles $1.tex\
+pdflatex -halt-on-error $1tex\
 | grep -a3 ^!
 greprc=$?
 if [[ $greprc -eq 0 ]] ; then
@@ -33,4 +33,7 @@ fi
 echo Final compilation of pdflatex successful
 # move the pdf to the cwd
 
-mv intfiles/$1.pdf $1.pdf
+mv $1aux intfiles/
+mv $1bbl intfiles/
+mv $1blg intfiles/
+mv $1log intfiles/
